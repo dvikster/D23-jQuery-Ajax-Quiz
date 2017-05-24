6c0c3318afd2d48ad22b5ebe94a7d8c44fe87d66
dvikster <dvikster@gmail.com>
2017-05-24 16:06:37 +0300

    upd

--- js/js.js
+++ js/js.js
@@ -1,5 +1,26 @@
-
-$(document).ready(function(){
-
+$(document).ready(function () {
+        $('#send-answer').on('click', myQuiz);
 });
+
+function myQuiz() {
+    // answ1 = $('#q1').val();
+      $.post(
+        //куда
+        'quiz.php',
+        //что
+        {
+            'userName': $('#user-name').val(),
+            'userAge': $('#user-age').val(),
+            'q1': $('#q1').val(),
+            'q2': $('#q2').val()
+        },
+        //и что потом
+        ifSuccess
+    );
+}
+
+function ifSuccess(data) {
+    //все данные от сервера передаются в виде строки в переменную data
+    $('#server-answer').html(data);
+}
 
